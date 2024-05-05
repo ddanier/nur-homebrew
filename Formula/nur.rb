0286a3ca824697264f6f293d50aac4b3b083bd47
David Danier <danier@team23.de>
2024-05-05 14:21:35 +0200

    nur 0.4.1

--- Formula/nur.rb
+++ Formula/nur.rb
@@ -6,8 +6,8 @@
 class Nur < Formula
   desc "Task runner based on nu shell"
   homepage "https://github.com/ddanier/nur"
-  url "https://github.com/ddanier/nur/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "8d27dec117035ff65dbbf42ff70f53e7cd8675a5d595ec1cec0631ba8f3586e1"
+  url "https://github.com/ddanier/nur/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "2784ca70eedbf4bf6e6aebb2f2aaa2a216b6074ec97f717341e1c152e865bf29"
   license "MIT"
   head "https://github.com/ddanier/nur.git", branch: "main"
 
@@ -33,6 +33,6 @@ class Nur < Formula
   end
 
   test do
-    assert_match "0.4.0", pipe_output("#{bin}/nur --version", nil)
+    assert_match "0.4.1", pipe_output("#{bin}/nur --version", nil)
   end
 end
